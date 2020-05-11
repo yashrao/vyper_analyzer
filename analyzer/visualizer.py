733a23068f361e0682a0b092396ad13682c17807
Yash Rao <12raoy1@gmail.com>
2020-05-12 03:22:13 +1000

    new func get_variable and made it shorter

--- analyzer/visualizer.py
+++ analyzer/visualizer.py
@@ -36,27 +36,27 @@ class Visualizer:
         ast_type = line['ast_type']
         node_struct_str = ''
         if ast_type == 'Assign':
-            if len(body) == 1:
+            if line['value']['ast_type'] == 'Name':
                 node_struct_str += '{} {} {}'.format(
                     line['target']['attr'], 
                     AST_TYPES[ast_type], 
                     line['value']['id']) 
-            else:
-                node_struct_str += '{} {} {} |'.format(
+            elif line['value']['ast_type'] == 'Attribute':
+                # TODO: Change this for attributes of attributes
+                node_struct_str += '{} {} {}.{}'.format(
                     line['target']['attr'], 
                     AST_TYPES[ast_type], 
-                    line['value']['id']) 
+                    line['value']['attr'],
+                    line['value']['value']['id']) 
+            if len(body) > 1:
+                node_struct_str += ' |'
         elif ast_type == 'AugAssign':
             op = AST_TYPES['AugAssign'][line['op']['ast_type']]
-            if len(body) == 1:
-                node_struct_str += '{} {} {}'.format(
-                    line['target']['attr'], 
-                    op, line['value']['value']) 
-            else:
-                node_struct_str += '{} {} {} |'.format(
-                    line['target']['attr'], 
-                    op, 
-                    line['value']['value']) 
+            node_struct_str += '{} {} {}'.format(
+                line['target']['attr'], 
+                op, line['value']['value']) 
+            if len(body) > 1:
+                node_struct_str += ' |'
         return node_struct_str
     ##
     # Pass a list of FunctionDef nodes
@@ -67,47 +67,34 @@ class Visualizer:
           node_label = self.get_func_label(node['args']['args'], 
             node['decorator_list'], 
             node['name']) 
-          for line in node['body']:
-              if 'target' in line.keys():
-                pprint(line)
-                sg.attr(label=node_label, 
-                    style='dashed', 
-                    color='black', 
-                    labelloc='t', 
-                    rank='same')
-                sg.node(node['name'], 'ENTRY', shape='Mdiamond')
-                node_struct_str = '{ '
-                ast_type = line['ast_type']
-                body = node['body']
-                if ast_type == 'AugAssign':
-                  op = AST_TYPES['AugAssign'][line['op']['ast_type']]
-                  if len(node['body']) == 1:
-                    #node_struct_str += '{} {} {}'.format(
-                    #    line['target']['attr'], 
-                    #    op, line['value']['value']) 
-                    node_struct_str += self.get_variable(line, body)
-                  else:
-                    node_struct_str += self.get_variable(line, body)
-                  node_struct_str += '}'
-                  sg.node_attr = {'shape': 'record'}
-                  #making sure its formatted as a raw string
-                  sg.node('struct_' + node['name'], 
+        for line in node['body']:
+            #TODO: Try/Except for debug/error handling
+            try:
+                if 'target' in line.keys():
+                    sg.attr(label=node_label, 
+                        style='dashed', 
+                        color='black', 
+                        labelloc='t', 
+                        rank='same')
+                    sg.node(node['name'], 'ENTRY', shape='Mdiamond')
+                    node_struct_str = '{ '
+                    ast_type = line['ast_type']
+                    body = node['body']
+                    node_struct_str = self.get_variable(line, body)
+                        
+                    node_struct_str += '}'
+                    sg.node_attr = {'shape': 'record'}
+                    #make sure its formatted as a raw string
+                    sg.node('struct_' + node['name'], 
                       r'{}'.format(node_struct_str)) 
-                elif ast_type == 'Assign':
-                  if len(node['body']) == 1:
-                    node_struct_str += self.get_variable(line, body) 
-                  else:
-                    node_struct_str += self.get_variable(line, body)
-                  node_struct_str += '}'
-                  sg.node_attr = {'shape': 'record'}
-                  #make sure its formatted as a raw string
-                  sg.node('struct_' + node['name'], 
-                    r'{}'.format(node_struct_str)) 
-
-                sg.node(node['name'] + '_exit', 'EXIT', shape='Mdiamond')
-                sg.edge(node['name'], 'struct_' + node['name'])
-                sg.edge('struct_' + node['name'], node['name'] + '_exit')
-                sg.edge_attr.update(color='blue', weight='100')
+                   
+                    sg.node(node['name'] + '_exit', 'EXIT', shape='Mdiamond')
+                    sg.edge(node['name'], 'struct_' + node['name'])
+                    sg.edge('struct_' + node['name'], node['name'] + '_exit')
+                    sg.edge_attr.update(color='blue', weight='100')
+            except KeyError as e:
+                pprint(line)
+                raise e
               
       self._graph.view() # TODO: Remove
 
